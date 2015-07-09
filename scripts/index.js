be5ccfc60cb08e257fa099c64c06dec9a8fec30c
Valentin Vago <zeropaper@irata.ch>
2015-07-09 08:12:59 +0200

    externalize index.html scripts

--- scripts/index.js
+++ scripts/index.js
@@ -7,20 +7,3 @@ require('./clausevalues-setter-view');
 require('./clauseexpression-setter-view');
 
 module.exports = DecisionTableView;
-
-function nodeListarray(els) {
-  if (Array.isArray(els)) {
-    return els;
-  }
-  var arr = [];
-  for (var i = 0; i < els.length; i++) {
-    arr.push(els[i]);
-  }
-  return arr;
-}
-
-function selectAll(selector, ctx) {
-  ctx = ctx || document;
-  return nodeListarray(ctx.querySelectorAll(selector));
-}
-window.selectAll = selectAll;
